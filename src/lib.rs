7e084e76359188977184807b0882048a2d3d5547
Erkki Silvola <eki833@gmail.com>
2026-02-17 21:11:31 +0200

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # serialport-stream
 //!
 //! Pure event driven implementation of futures::Stream for reading data from serialport utilizing [serialport-rs](https://github.com/serialport/serialport-rs).
-//! Produces 1-N amount of bytes depending on polling interval. Poll next will indefinitely wait for data in event, error or drop.
+//! Produces 1-N amount of bytes depending on polling interval. Initial poll starts background thread which will indefinitely wait for data in event, error or drop.
 //!
 
 use std::pin::Pin;
